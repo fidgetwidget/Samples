bd193adbbb35c552691e3e4182e7265dda10fc92
Brandon Bey <brandonbey@gmail.com>
2018-03-26 13:17:34 -0700

    Expanding on the Posts slugify hook
    - prevent collision of slug

--- adonis/app/Models/Hooks/Post.js
+++ adonis/app/Models/Hooks/Post.js
@@ -3,15 +3,22 @@
 const slugify = use('slugify')
 
 const Page = use('App/Models/Page')
+const Post = use('App/Models/Post')
 
 const PostHook = module.exports = {}
 
 PostHook.createSlug = async (postInstance) => {
+  const page = await Page.find(postInstance.page_id)
+
   if (postInstance.title) {
-    postInstance.slug = slugify(postInstance.title)
+    let slug = slugify(postInstance.title)
+    const count = await Post.query().where('slug', 'like', slug+'%').getCount()
+    if (count > 0) {
+      slug += `-${count}`
+    }
+    postInstance.slug = slug
   } else {
-    const page = await Page.find(postInstance.page_id).with('posts')
-    const count = page.posts.length + 1
+    const count = Post.query().where('page_id', page.id).getCount() + 1
     postInstance.slug = page.name + '-' + count
   }
 }
