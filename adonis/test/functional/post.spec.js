10871a66d6a2c5397de448838fb1b727a134ef89
Brandon Bey <brandonbey@gmail.com>
2018-03-28 13:37:52 -0700

    Adding before action to posts spec
    - ensure there is a page and user for the post before running the tests
    - added create then delete test

--- adonis/test/functional/post.spec.js
+++ adonis/test/functional/post.spec.js
@@ -1,15 +1,20 @@
 'use strict'
 
-const { test, trait } = use('Test/Suite')('Post')
+const { test, before, trait } = use('Test/Suite')('Post')
 
 const Post = use('App/Models/Post')
+const Page = use('App/Models/Page')
+const User = use('App/Models/User')
+
+let user_id = 1
+let page_id = 1
 
 trait('Test/ApiClient')
 
 test('get list of posts', async ({ client }) => {
   await Post.create({
-    page_id: 1,
-    user_id: 1,
+    page_id,
+    user_id,
     title: 'Post Title',
     content: 'Post content'
   })
@@ -21,4 +26,29 @@ test('get list of posts', async ({ client }) => {
     title: 'Post Title',
     content: 'Post content'
   }])
+})
+
+test('create then delete a post', async ({ client }) => {
+  const post = await Post.create({
+    page_id,
+    user_id,
+    title: 'Post Title',
+    content: 'Post content'
+  })
+
+  const confirmCreateRes = await client.get(`api/v1/pages/${page_id}/posts`).end()
+
+  confirmCreateRes.assertStatus(200)
+  confirmCreateRes.assertJSONSubset([{
+    id: post.id,
+    title: 'Post Title',
+    content: 'Post content'
+  }])
+
+  const confirmDeleteRes = await client.delete(`api/v1/pages/${page_id}/posts/${post.id}`).end()
+
+  confirmDeleteRes.assertStatus(200)
+  confirmDeleteRes.assertJSONSubset({
+    success: true,
+  })
 })
