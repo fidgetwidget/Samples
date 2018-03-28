f458bee5c9148ddd660a913b63185fbf67fb96bf
Brandon Bey <brandonbey@gmail.com>
2018-03-28 14:44:34 -0700

    Post spec hard coded user & page id change
    - pull them from a config
    - use before to ensure they are valid, and create ones if they are not

---
+++ adonis/config/testing.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  page_id: 1,
+  user_id: 1,
+}
--- adonis/test/functional/post.spec.js
+++ adonis/test/functional/post.spec.js
@@ -1,15 +1,45 @@
 'use strict'
 
+// includes
 const { test, before, trait } = use('Test/Suite')('Post')
-
+const Config = use('Config')
 const Post = use('App/Models/Post')
 const Page = use('App/Models/Page')
 const User = use('App/Models/User')
 
-let user_id = 1
-let page_id = 1
-
 trait('Test/ApiClient')
+
+// global values used across multiple tests
+let user_id, page_id
+
+before(async () => {
+  user_id = Config.get('testing.user_id')
+  page_id = Config.get('testing.page_id')
+  let user, page
+  
+  // Ensure we have a user for the given user_id
+  user = await User.find(user_id)
+  if (!user) {
+    user = await User.create({
+      username: `post-spec-user-${+Date.now()}`,
+      email: `brandonbey+${+Date.now()}@gmail.com`,
+      password: 'pwd'
+    })
+    user_id = user.id
+  }
+
+  // Ensure we have a page for the given page_id
+  page = await Page.find(page_id)
+  if (!page) {
+    page = await Page.create({
+      name: `post-spec-page-${+Date.now()}`,
+      title: 'Post Spec Test Page',
+      content: ''
+    })
+    page_id = page.id
+  }
+
+})
 
 test('get list of posts', async ({ client }) => {
   await Post.create({
